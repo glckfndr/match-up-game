a168f391004de2121a28c88512bd4b890626b182
glckfndr <obulan65@gmail.com>
2024-07-20 18:51:00 +0300

    refactor: add constants file

---
+++ constants/ornaments.js
@@ -0,0 +1,18 @@
+export const ornaments = [
+  { img: require("../assets/p56_1.svg"), name: "хрещик" },
+  { img: require("../assets/p56_2.svg"), name: "млинок" },
+  { img: require("../assets/p56_3.svg"), name: "колісниця" },
+  { img: require("../assets/p56_4.svg"), name: "сонечко" },
+  { img: require("../assets/p56_5.svg"), name: "косиця" },
+  { img: require("../assets/p56_6.svg"), name: "віночок" },
+  { img: require("../assets/p56_8.svg"), name: "вітрячок" },
+  { img: require("../assets/p56_9.svg"), name: "гачки" },
+
+  // require("../assets/p56_7.svg"), // сонечко
+  // require("../assets/p56_10.svg"), // ружа
+  // require("../assets/p56_11.svg"), // ружа
+  // require("../assets/p56_12.svg"), // вітрячок
+];
+
+export const WIDTH = 80;
+export const HEIGHT = 80;
